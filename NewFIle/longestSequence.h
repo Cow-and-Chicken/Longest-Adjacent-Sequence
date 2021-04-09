45a99748d0928d6929f65379eb6402b13d67998b
AngelovD <71969791+AngelovD@users.noreply.github.com>
2021-04-09 23:07:23 +0300

    Add files via upload

---
+++ NewFIle/longestSequence.h
@@ -0,0 +1,9 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+
+int findLongestSequence(char **matrix,int rows,int cols);
+static int getRecursiveRow(char **matrix,char color, int rows,int cols, int fnRow, int currCol);
+static int getRecursiveCol(char **matrix,char color, int rows,int cols, int currRow, int fnCol);
+
+

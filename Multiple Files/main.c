0e2149b352a824ce2de932f5c6e3ad3b508751d6
Siyana Hristova <siyah.hristova@gmail.com>
2021-04-12 10:09:28 +0300

    Add files via upload

--- Multiple Files/main.c
+++ Multiple Files/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "longestSequence.h"
 
+void fileInput(char *name);
+
 int main(int argc, char *argv[])
 {
     if (argc <= 1)
@@ -9,42 +11,47 @@ int main(int argc, char *argv[])
         return 1;
     }
     int counter;
-    for (counter = 1; counter <= argc; counter++)
+    for (counter = 1; counter < argc; counter++)
     {
-        FILE *inputFilePtr;
-        int row, col;
-        inputFilePtr = fopen(argv[counter], "r");
-        if (inputFilePtr == NULL)
-        {
-            perror("Error in opening file");
-            exit(1);
-        }
-        fscanf(inputFilePtr, "%d ", &col);
-        fscanf(inputFilePtr, "%d ", &row);
-        char **resultsArr = (char **)malloc(row * sizeof(char *));
-
-        for (int i = 0; i < row; i++)
-        {
-            resultsArr[i] = (char *)malloc((col) * sizeof(char));
-        }
-        for (int i = 0; i < row; i++)
-        {
-            for (int j = 0; j < col; j++)
-            {
-                fscanf(inputFilePtr, "%c ", &resultsArr[i][j]);
-            }
-        }
-        int result = findLongestSequence(resultsArr, row, col);
-        printf("%d. Result: %d\n", counter, result);
-
-        for (int i = 0; i < row; i++)
-        {
-            free(resultsArr[i]);
-        }
-
-        free(resultsArr);
-        fclose(inputFilePtr);
-        inputFilePtr = NULL;
+        printf("%d. ", counter);
+        fileInput(argv[counter]);
     }
     return 0;
+}
+void fileInput(char *name)
+{
+    FILE *inputFilePtr;
+    int row, col;
+    inputFilePtr = fopen(name, "r");
+    if (inputFilePtr == NULL)
+    {
+        perror("Error in opening file");
+        exit(1);
+    }
+    fscanf(inputFilePtr, "%d ", &col);
+    fscanf(inputFilePtr, "%d ", &row);
+    char **resultsArr = (char **)malloc(row * sizeof(char *));
+
+    for (int i = 0; i < row; i++)
+    {
+        resultsArr[i] = (char *)malloc((col) * sizeof(char));
+    }
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            fscanf(inputFilePtr, "%c ", &resultsArr[i][j]);
+        }
+    }
+    int result = findLongestSequence(resultsArr, row, col);
+    printf("Result %d\n", result);
+
+    for (int i = 0; i < row; i++)
+    {
+        free(resultsArr[i]);
+    }
+
+    free(resultsArr);
+    fclose(inputFilePtr);
+    inputFilePtr = NULL;
 }
